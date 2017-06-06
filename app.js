c0b4c3defef81cc370efaab2f8304fd60a0a7459
0peron <phubbard89@gmail.com>
2017-06-05 20:31:31 -0400

    added modal functionality; need to work on smoothscroll next

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var modal = document.getElementById('myModal');
-var btn = document.getElementById("modalBtn");
-var span = document.getElementsByClassName("close")[0];
-btn.onclick = function () {
-    modal.style.display = "block";
-}
-span.onclick = function () {
-    modal.style.display = "none";
-}
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+window.onload = function () {
+    $(".modalBtn").on("click", function () {
+        var modal = $(this).data("modal");
+        $(modal).show();
+    });
+
+    $(".modal").on("click", function (e) {
+        var className = e.target.className;
+        if (className === "modal" || className === "close") {
+            $(this).closest(".modal").hide();
+        }
+    });
+
 };
