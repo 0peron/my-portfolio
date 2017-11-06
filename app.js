77ebfb11cebfeb70c8420a84dd8b907d36e40329
0peron <phubbard89@gmail.com>
2017-11-06 17:09:36 -0500

    css and html cleanup

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 window.onload = function () {
-    $(".modalBtn").on("click", function () {
-        var modal = $(this).data("modal");
-        $(modal).show();
-    });
-
-    $(".modal").on("click", function (e) {
-        var className = e.target.className;
-        if (className === "modal" || className === "close") {
-            $(this).closest(".modal").hide();
-        }
-    });
+//    $(".modalBtn").on("click", function () {
+//        var modal = $(this).data("modal");
+//        $(modal).show();
+//    });
+//
+//    $(".modal").on("click", function (e) {
+//        var className = e.target.className;
+//        if (className === "modal" || className === "close") {
+//            $(this).closest(".modal").hide();
+//        }
+//    });
 
     $('a[href*="#"]')
         .not('[href="#"]')
